c53abb28566e892a07032d885f9c08fac34c2cad
Raul Gomez <cronosworld@gmail.com>
2021-01-15 19:52:26 +0100

    refactors, phpdoc

--- src/Command/TSPCommand.php
+++ src/Command/TSPCommand.php
@@ -6,28 +6,37 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
 
-
+/**
+ * TSPCommand
+ */
 class TSPCommand extends Command
 {
-    protected static $defaultName = 'app:tsp';
-
-    protected function configure()
+    protected static string $defaultName = 'app:tsp';
+    
+    /**
+     * configure
+     *
+     * @return void
+     */
+    protected function configure(): void
     {
         $this
             ->setDescription('Solves the TSP according to the cities.txt file stored on src/Business/Documents.')
             ->setHelp('Just run the command!');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+        
+    /**
+     * execute
+     *
+     * @param  mixed $input
+     * @param  mixed $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $finder = new Finder();
-        $finder->in('src/Business/Documents')->name('cities.txt');
-        foreach($finder as $file) {
-            $contents = $file->getContents();
-        }
-       
-        die(var_dump($contents));
-
+        $cities = $this->getCities(); 
+        die(var_dump($cities));
         // return this if there was no problem running the command
         // (it's equivalent to returning int(0))
         return Command::SUCCESS;
@@ -36,4 +45,21 @@ class TSPCommand extends Command
         // (it's equivalent to returning int(1))
         // return Command::FAILURE;
     }
+
+        
+    /**
+     * getCities
+     *
+     * @return string
+     */
+    private function getCities(): string {
+        $contents = [];
+        $finder = new Finder();
+        $finder->in('src/Business/Documents')->name('cities.txt');
+        foreach($finder as $file) {
+            $contents = $file->getContents();
+        }
+
+        return $contents;
+    } 
 }
